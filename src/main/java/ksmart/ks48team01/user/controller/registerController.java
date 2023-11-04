6e1d3250ce6222bea1a6820859362de295fdf718
椅子んほ君 <dooftac712091@gmail.com>
2023-11-04 16:41:53 +0900

    상단 navbar의 로고를 변경, 유저 페이지를 추가했습니다.

--- src/main/java/ksmart/ks48team01/user/controller/registerController.java
+++ src/main/java/ksmart/ks48team01/user/controller/registerController.java
@@ -3,29 +3,30 @@ package ksmart.ks48team01.user.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("registController")
-@RequestMapping("/user/regist")
-public class RegistController {
+@RequestMapping("/user/register")
+public class registerController {
 
     // 가입 할 회원 권한을 선택할 수 있는 페이지
     // 권한 버튼에 따라서 쿼리스트링의 값이 바뀌고, 그 입력값에 따라 다른 가입 Form을 가져온다.
-    @GetMapping(value = "/registType")
-    public String registType(Model model) {
+    @GetMapping(value = "/registerType")
+    public String registerType(Model model) {
 
         model.addAttribute("title", "회원선택 - 누리컬쳐");
 
-        return "user/regist/registTerms";
+        return "user/register/registerType";
     }
 
     // 회원 가입 시 이용약관의 동의를 체크하는 페이지
-    @GetMapping(value = "/registTerms")
-    public String registTerms(Model model) {
+    @GetMapping(value = "/registerTerms")
+    public String registerTerms(Model model) {
 
         model.addAttribute("title", "약관동의 - 누리컬쳐");
 
-        return "user/regist/registTerms";
+        return "user/register/registerTerms";
     }
 
     // Nuriculture 웹 애플리케이션을 이용하는 사용자(User) 권한의 가입 Form
@@ -34,7 +35,7 @@ public class RegistController {
 
         model.addAttribute("title", "회원가입 - 누리컬쳐");
 
-        return "user/regist/memberRegister";
+        return "user/register/memberRegister";
     }
 
     // Nuriculture 웹 애플리케이션에서 상품을 판매, 게시하는 가맹점(store) 권한의 가입 Form
@@ -43,7 +44,7 @@ public class RegistController {
 
         model.addAttribute("title", "회원가입 - 누리컬쳐");
 
-        return "user/regist/storeRegister";
+        return "user/register/storeRegister";
     }
 
     // Nuriculture 웹 애플리케이션을 이용, 관리하는 공무원(officer) 권한의 가입 Form
@@ -52,16 +53,18 @@ public class RegistController {
 
         model.addAttribute("title", "회원가입 - 누리컬쳐");
 
-        return "user/regist/officerRegister";
+        return "user/register/officerRegister";
     }
 
+
+
     // 회원가입이 완료되었다는 것을 표시하는 페이지
-    @GetMapping(value = "/registConfirm")
-    public String registConfirm(Model model) {
+    @GetMapping(value = "/registerConfirm")
+    public String registerConfirm(Model model) {
 
         model.addAttribute("title", "회원가입 - 누리컬쳐");
 
-        return "user/regist/registConfirm";
+        return "user/register/registerConfirm";
     }
 
 
