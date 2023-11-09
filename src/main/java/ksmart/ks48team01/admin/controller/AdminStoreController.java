fc937689447c2271b5543ab7fed9c30106c6f055
椅子んほ君 <dooftac712091@gmail.com>
2023-11-09 10:03:00 +0900

    AdminStoreController에서 CRUD를 생성했습니다.

--- src/main/java/ksmart/ks48team01/admin/controller/AdminStoreController.java
+++ src/main/java/ksmart/ks48team01/admin/controller/AdminStoreController.java
@@ -1,5 +1,7 @@
 package ksmart.ks48team01.admin.controller;
 
+import ksmart.ks48team01.service.StoreService;
+import ksmart.ks48team01.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +11,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin/store")
 public class AdminStoreController {
+
+
+    private final StoreService storeService;
+    private final UserService userService;
+
+    public AdminStoreController (StoreService storeService, UserService userService) {
+        this.storeService = storeService;
+        this.userService = userService;
+    }
+
+    /**
+     * 게시판 전체 목록 조회
+     * @return
+     */
+    @GetMapping("/storeInfoList")
+    public String storeInfoList() {
+
+        return "admin/store/storeInfoList";
+    }
+
+
+    @GetMapping("/storeInfoRegister")
+    public String storeInfoRegister() {
+
+        return "admin/store/storeInfoRegister";
+    }
+
+    @GetMapping("/storeInfoUpdate")
+    public String storeInfoUpdate() {
+
+        return "admin/store/storeInfoUpdate";
+    }
+
+
 
 }
