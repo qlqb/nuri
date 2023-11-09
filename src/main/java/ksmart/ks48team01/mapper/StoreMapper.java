1c7fec32f22e16465ab95042b110e68f540ad9dd
椅子んほ君 <dooftac712091@gmail.com>
2023-11-09 09:44:06 +0900

    log4j를 푸시 안하고 Merge를 해서 죄송합니다.

--- src/main/java/ksmart/ks48team01/mapper/StoreMapper.java
+++ src/main/java/ksmart/ks48team01/mapper/StoreMapper.java
@@ -17,15 +17,26 @@ public interface StoreMapper {
 
 
     /**
-     * 가맹점 소개 페이지에서 사용될 페이지 전체 조회
+     * 관리자 가맹점 관리 페이지 및 가맹점 소개 페이지에서 사용하기 위한 페이지 전체 조회
+     * 사용되는 페이지
+     * 관리자 : [가맹점 조회, 관리, 삭제]
+     * 사용자 : [가맹점 관리 페이지]
      * @return
      */
     public List<Store> getStoreAllKeyword();
 
     /**
-     *
-     * @param storeKeyword
+     * 해당 키워드(가맹점 카테고리)를 검색시에 해당되는 가맹점을 출력
+     * 사용되는 페이지 
+     * @param storeKeyword 해당 카테고리 키워드
      * @return
      */
     public List<Store> getStoreKeyword(String storeKeyword);
+
+    /**
+     * 관리자 페이지에서 수정을 위한
+     */
+    public Store getSpecificStore(String storeId);
+
+
 }
